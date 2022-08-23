5d5c49f8e3d9eb4cddc4bcf1b2a230a7a47036ce
nurak23 <91521374+nurak23@users.noreply.github.com>
2022-08-23 20:54:16 +0530

    Update index.js
    Timestamp and reset functions have been added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let count =0;
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
-
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
 
 function increment(){
     count += 1;
@@ -15,4 +20,8 @@ function save(){
     countEl.textContent=0;
     count=0;
 
+}
+
+function reset() {
+    document.location.reload()
 }
