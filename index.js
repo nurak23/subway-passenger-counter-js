4184996a9b79df8c464c9e241795164d29a57d8f
nurak23 <91521374+nurak23@users.noreply.github.com>
2022-08-23 19:37:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+let count =0;
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
+
+function increment(){
+    count += 1;
+    countEl.innerText = +count;
+}
+
+function save(){
+    let countStr = count +" - "
+    saveEl.textContent += countStr;
+    console.log(count);
+    countEl.textContent=0;
+    count=0;
+
+}
